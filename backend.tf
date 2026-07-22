dcb2699feb697e80a72d9653a04afb4efb5ce7f1
gayatrisalokhe104 <168088518+gayatrisalokhe104@users.noreply.github.com>
2026-07-22 11:21:58 +0530

    Update S3 backend configuration in backend.tf

--- backend.tf
+++ backend.tf
@@ -1,19 +1,16 @@
 terraform {
   required_version = ">= 1.3.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-
   backend "s3" {
-    bucket         = "young-minds-terraform-tfstate"
-    region         = "us-east-1"
-    key            = "batch-32/terraform.tfstate"
-    dynamodb_table = "terraform-lock"   
-    encrypt        = true             
+    bucket       = "young-minds-terraform-tfstate"
+    region       = "us-east-1"
+    key          = "batch-32/terraform.tfstate"
+    use_lockfile = true
+    encrypt      = true
   }
 }
-
